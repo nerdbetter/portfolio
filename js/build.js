bf74b1221556840aa90dd1111b083684dcb46431
nerdbetter <jasontlogan@gmail.com>
2017-07-11 23:15:19 -0500

    DOM manipulation builtbut not running correctly

--- js/build.js
+++ js/build.js
@@ -1,18 +1,34 @@
 ('use strict');
 
 var projects = [];
-function Project(preview, title, src, languages, authors, description){
-  this.preview = rawData.preview;
-  this.title = rawData.title;
-  this.source = rawData.source;
-  this.languages = rawData.languages;
-  this.authors = rawData.authors;
-  this.description = rawData.description;
+
+function Project(rawDataObj){
+  this.preview = rawDataObj.preview;
+  this.title = rawDataObj.title;
+  this.source = rawDataObj.source;
+  this.languages = rawDataObj.languages;
+  this.authors = rawDataObj.authors;
+  this.description = rawDataObj.description;
 };
 
 Project.prototype.toHtml = function() {
-  var $Projects = $('article.template').clone();
-  $Projects.toggleClass('template');
+  var $newProject = $('article.template').clone();
+  $newProject.toggleClass('template');
 
-
+  //$newProject.find('article a').attr(this.source);
+  //$newProject.find('article a').text(this.source);
+  $newProject.find('artice h1').text(this.title);
+  $newProject.find('article h2').text(this.authors);
+  $newProject.find('article h3').text(this.languages);
+  $newProject.find('article p').text(this.description);
+  return $newProject;
 };
+
+rawData.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+  console.log(projects);
+});
+
+projects.forEach(function(project) {
+  $('#projects').append(project.toHtml());
+});
--- js/projectObjects.js
+++ js/projectObjects.js
@@ -2,11 +2,28 @@
 
 var rawData = [
   {
-    preview:  url('images/attackerator.png'),
-    name: 'Attackerator',
+    preview: 'images/attackerator.png',
+    title: 'Attackerator',
+    source: 'https://nerdbetter.github.io/projectWeek/',
+    languages: 'JavaScript, HTML5, CSS3',
+    authors: 'Jason Logan, David Becker, Mitchell Brock',
+    description: '<p>Hodor. Hodor HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor hodor; hodor hodor?! Hodor HODOR hodor, hodor hodor hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor; hodor hodor - hodor hodor hodor. Hodor. Hodor. Hodor hodor hodor hodor... Hodor hodor hodor. Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor.Hodor. Hodor hodor; hodor hodor, hodor. Hodor hodor. Hodor. Hodor hodor hodor hodor... Hodor hodor HODOR hodor, hodor hodor? Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor hodor! Hodor! Hodor hodor, HODOR hodor, hodor hodor - hodor - hodor hodor hodor hodor! Hodor, hodor; hodor hodor, hodor. Hodor hodor hodor?! Hodor. Hodor hodor, hodor. Hodor hodor. Hodor hodor HODOR! Hodor hodor, hodor. Hodor hodor - hodor - hodor. </p>'
+  },
+  {
+    preview: 'images/attackerator.png',
+    title: 'Attackerator',
+    source: 'https://nerdbetter.github.io/projectWeek/',
+    languages: 'JavaScript, HTML5, CSS3',
+    authors: 'Jason Logan, David Becker, Mitchell Brock',
+    description: '<p>Hodor. Hodor HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor hodor; hodor hodor?! Hodor HODOR hodor, hodor hodor hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor; hodor hodor - hodor hodor hodor. Hodor. Hodor. Hodor hodor hodor hodor... Hodor hodor hodor. Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor.Hodor. Hodor hodor; hodor hodor, hodor. Hodor hodor. Hodor. Hodor hodor hodor hodor... Hodor hodor HODOR hodor, hodor hodor? Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor hodor! Hodor! Hodor hodor, HODOR hodor, hodor hodor - hodor - hodor hodor hodor hodor! Hodor, hodor; hodor hodor, hodor. Hodor hodor hodor?! Hodor. Hodor hodor, hodor. Hodor hodor. Hodor hodor HODOR! Hodor hodor, hodor. Hodor hodor - hodor - hodor. </p>'
+  },
+  {
+    preview: 'images/attackerator.png',
+    title: 'Attackerator',
     source: 'https://nerdbetter.github.io/projectWeek/',
     languages: 'JavaScript, HTML5, CSS3',
     authors: 'Jason Logan, David Becker, Mitchell Brock',
     description: '<p>Hodor. Hodor HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor hodor; hodor hodor?! Hodor HODOR hodor, hodor hodor hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor; hodor hodor - hodor hodor hodor. Hodor. Hodor. Hodor hodor hodor hodor... Hodor hodor hodor. Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor.Hodor. Hodor hodor; hodor hodor, hodor. Hodor hodor. Hodor. Hodor hodor hodor hodor... Hodor hodor HODOR hodor, hodor hodor? Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor hodor! Hodor! Hodor hodor, HODOR hodor, hodor hodor - hodor - hodor hodor hodor hodor! Hodor, hodor; hodor hodor, hodor. Hodor hodor hodor?! Hodor. Hodor hodor, hodor. Hodor hodor. Hodor hodor HODOR! Hodor hodor, hodor. Hodor hodor - hodor - hodor. </p>'
   }
 ];
+console.log(rawData);
